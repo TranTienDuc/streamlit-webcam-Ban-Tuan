5ba4eaa8db82f4aba9c29f548b8d8caa30d750de
TranTienDuc <trantienduc@gmail.com>
2025-04-29 19:50:16 +0700

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -235,6 +235,26 @@ class Inference:
             # Configure WebRTC
             # Thay đổi phần cấu hình WebRTC trong phương thức inference()
             rtc_configuration = RTCConfiguration({
+                                "iceServers":[ 
+                                        {'url': 'stun:global.stun.twilio.com:3478', 
+                                         'urls': 'stun:global.stun.twilio.com:3478'
+                                        }, 
+                                        {'credential': '+AhWhqdmbiYjA/F2YuDkLhKm8CbxMTazACnzhUIT164=', 
+                                         'url': 'turn:global.turn.twilio.com:3478?transport=udp', 
+                                         'urls': 'turn:global.turn.twilio.com:3478?transport=udp', 
+                                         'username': '5b29b713bcdbdf04d16795646c8d43304ac49e6b381f82778f3d8b0861b21c76'
+                                        }, 
+                                        {'credential': '+AhWhqdmbiYjA/F2YuDkLhKm8CbxMTazACnzhUIT164=', 
+                                         'url': 'turn:global.turn.twilio.com:3478?transport=tcp', 
+                                         'urls': 'turn:global.turn.twilio.com:3478?transport=tcp', 
+                                         'username': '5b29b713bcdbdf04d16795646c8d43304ac49e6b381f82778f3d8b0861b21c76'
+                                        }, 
+                                        {'credential': '+AhWhqdmbiYjA/F2YuDkLhKm8CbxMTazACnzhUIT164=', 
+                                         'url': 'turn:global.turn.twilio.com:443?transport=tcp', 
+                                         'urls': 'turn:global.turn.twilio.com:443?transport=tcp', 
+                                         'username': '5b29b713bcdbdf04d16795646c8d43304ac49e6b381f82778f3d8b0861b21c76'
+                                        }
+                                      ]
                 }
             )
             
